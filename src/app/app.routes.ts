5c9a62ac7cba8a232b6bfe9d0cce3b981fa17cec
André Coutinho de Macedo Silva <andre.coutinhom@gmail.com>
2025-11-22 03:57:06 -0300

    Novo estilo iniciado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 import { CadastroComponent } from './paginas/cadastro/cadastro.component';
 import { ConsultaComponent } from './paginas/consulta/consulta.component';
 import { ExclusaoComponent } from './paginas/exclusao/exclusao.component';
 import { ListagemComponent } from './paginas/listagem/listagem.component';
 
 export const routes: Routes = [
-    {path: '', component: CadastroComponent},
+    {path: '', component: MainComponent},
     {path: 'cadastro', component: CadastroComponent, title: 'Cadastro'},
     {path: 'consulta', component: ConsultaComponent, title: 'Consulta'},
     {path: 'exclusao', component: ExclusaoComponent, title: 'Exclusão'},
